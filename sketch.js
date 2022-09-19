cf637146389e3cbc1c731f1b15fadf276ad53edb
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-09-19 18:05:04 +0200

    Update sketch.js
    Added function to draw lines from a set of attributes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,27 @@
+//array posto a contenere i riferimenti
+//virtuali ai cerchi in movimento
+var circles = [];
+
+//array posto a contenere le specifiche
+//per creare le linee
+var lines = [];
+
 function preload() {
   // put preload code here
+  lines = [
+    {
+    x1: 0, x2: 1,
+    y1: 0.45, y2: 0.8,
+    color: color(126, 75, 114),
+    weight: 7
+    },
+    {
+      x1: 0, x2: 1,
+      y1: 0.25, y2: 0.6,
+      color: color(240, 75, 114),
+      weight: 7
+    }
+  ]
 }
 
 function setup() {
@@ -9,5 +31,32 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background("green");
+  background(230,209,94);
+
+  lines.forEach(element => drawLine(element))
+  
+  noStroke()
+  fill(255, 0, 0)
+  circle()
+}
+
+//funzione per creare linee in base
+//alle specifiche di lines[]
+function drawLine(obj) {
+  console.log(obj)
+  if (obj.color) {
+    stroke(obj.color)
+  }
+  else {
+    stroke(0)
+  }
+
+  if (obj.weight) {
+    strokeWeight(obj.weight)
+  }
+  else {
+    strokeWeight(5)
+  }
+
+  line(obj.x1*windowWidth, obj.y1*windowHeight, obj.x2*windowWidth, obj.y2*windowHeight)
 }
