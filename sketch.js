cbeede025865efc8be125a06c901f8c2e0b0f31f
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-09-27 08:50:06 +0200

    Update sketch.js
    -Added function to create virtual circles
    -Begun work on assigning movement lines to virtual circles
    -Honestly just needed to switch workstation

--- sketch.js
+++ sketch.js
@@ -10,16 +10,20 @@ function preload() {
   // put preload code here
   lines = [
     {
-    x1: 0, x2: 1,
-    y1: 0.45, y2: 0.8,
-    color: color(126, 75, 114),
-    weight: 7
+      x1: 0, x2: 1,
+      y1: 0.45, y2: 0.8,
+      color: color(126, 75, 114),
+      weight: 7,
+      isHorizontal: 1,
+      startLeft: 1
     },
     {
       x1: 0, x2: 1,
       y1: 0.25, y2: 0.6,
       color: color(240, 75, 114),
-      weight: 7
+      weight: 7,
+      isHorizontal: 1,
+      startLeft: 1
     }
   ]
 }
@@ -43,7 +47,7 @@ function draw() {
 //funzione per creare linee in base
 //alle specifiche di lines[]
 function drawLine(obj) {
-  console.log(obj)
+  //se non è definito un colore, imposta il nero
   if (obj.color) {
     stroke(obj.color)
   }
@@ -51,6 +55,7 @@ function drawLine(obj) {
     stroke(0)
   }
 
+  //se non è definito un peso, imposta 5
   if (obj.weight) {
     strokeWeight(obj.weight)
   }
@@ -58,5 +63,62 @@ function drawLine(obj) {
     strokeWeight(5)
   }
 
+  //disegna la linea con riferimento alle dimensioni della finestra
   line(obj.x1*windowWidth, obj.y1*windowHeight, obj.x2*windowWidth, obj.y2*windowHeight)
+}
+
+//funzione che crea linee di direzione e colore casuale
+function randomLine() {
+
+  let randLine = {
+    x1: 0, x2: 0,
+    y1: 0, y2: 0,
+    color: color(126, 75, 114),
+    weight: 7,
+    isHorizontal: 1,
+    startLeft: 1
+  }
+
+  let orientation = Math.round(random());
+  let direction = Math.round(random());
+
+  if (orientation) {
+    
+  }
+  else {
+    
+  }
+}
+//funzione che crea cerchi di dimensione e colore casuale
+function randomCircle() {
+  const maxRadius = 200;
+  const minRadius = 100;
+
+  //unità al secondo 
+  const maxSpeed = 300;
+  const minSpeed = 80;
+
+  let randCircle = {
+    radius: random(minRadius, maxRadius),
+    color: color(random(0, 255), random(0, 255), random(0, 255)),
+    center: { x: 0, y: 0 },
+    speed: random(minSpeed, maxSpeed)/frameRate(),
+    angle: 0,
+    distance: 0
+    }
+  
+  return randCircle
+}
+
+//funzione che associa a un cerchio una linea da seguire
+function assignCircle(obj, follow) {
+  let slope = (follow.y1 - follow.y2) / (follow.x1 - follow.x2)
+
+  console.log(slope)
+  console.log(Math.atan(slope))
+}
+
+//funzione che aggiorna il movimento dei cerchi
+function updateCircle(obj) {
+
 }
